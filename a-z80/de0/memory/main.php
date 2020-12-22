f616b03df923fdabbaf22328a64bb650e4d55f58
FoxLib <hplus.foxtail@gmail.com>
2020-12-22 06:28:11 +0300

    OK

--- a-z80/de0/memory/main.php
+++ a-z80/de0/memory/main.php
@@ -1,5 +1,5 @@
 <?php
-$data = file_get_contents("zxdump.bin");
+$data = file_get_contents("zxrom.bin") . file_get_contents("zxvid.bin");
 $out = "WIDTH=8;\nDEPTH=65536;\nADDRESS_RADIX=HEX;\nDATA_RADIX=HEX;\nCONTENT BEGIN\n";
 for ($i = 0; $i < 0x5B00; $i++) $out .= sprintf("  %02X: %02X;\n", $i, ord($data[$i]));
 $out .= "  [5B00..FFFF]: 00;\n";
