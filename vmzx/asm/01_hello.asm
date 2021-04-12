95934ca33f3ddf956a1da11ec8abdac9b80da57e
FoxLib <hplus.foxtail@gmail.com>
2021-04-12 15:03:59 +0300

    Автозагрузка бейсик-программы

--- vmzx/asm/01_hello.asm
+++ vmzx/asm/01_hello.asm
@@ -1,6 +1,10 @@
 
         org     8000h
-
+        ld      b, 64
         ld      a, 0x21
+L1:     push    af
         rst     $10
+        pop     af
+        inc     a
+        djnz    L1
         ret
